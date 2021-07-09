dab69d10907d47e270a84c8e3c7a9994b2a79bc3
bhupendra.87 <yadavbhupendra787@gmail.com>
2021-07-09 06:14:57 +0530

    end of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable, Subscriber } from 'rxjs';
 import { AccountService } from 'src/_services/account.service';
 import { User } from '../_models/user';
@@ -11,7 +13,8 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any= {};
   // currentUser$: Observable<User>;
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService,private router: Router,
+    private toastr:ToastrService) { }
 
   ngOnInit(): void {
     //this.getCurrentUser();
@@ -21,13 +24,18 @@ export class NavComponent implements OnInit {
   login(){
     console.log(this.model);
     this.accountService.login(this.model).subscribe(response=>{
+      this.router.navigateByUrl('/members');
       console.log(response);
       //this.loggedIn = true;
-    })
+    },error=>{
+      console.log(error);
+      this.toastr.error(error.error);
+    });
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
     //this.loggedIn = false;
 
   }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { error } from 'protractor';
 import { AccountService } from 'src/_services/account.service';
 
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   // @Input() usersFromHomeComponent:any;
   @Output() cancelRegister = new EventEmitter();
   model:any = {};
-  constructor(private accountService:AccountService) {
+  constructor(private accountService:AccountService,private toastr:ToastrService) {
 
   }
 
@@ -25,7 +26,8 @@ export class RegisterComponent implements OnInit {
      this.cancel();
    },error=>{
     console.log(error);
-   })
+      this.toastr.error(error.error);
+   });
   }
 
   cancel(){
